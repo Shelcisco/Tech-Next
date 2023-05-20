d400738e79626477821aa250b31e0c1ff1864830
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-05-19 20:41:47 -0700

    adding models

---
+++ models/Blog.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {
+ 
+}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+       defaultValue: DataTypes.NOW,
+      },
+
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Blog;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,40 @@
 const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
 
-module.exports = { User };
+User.hasMany(Blog, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+
+})
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+
+})
+
+Blog.hasMany(Comment, {
+    foreignKey: "blog_id",
+    onDelete: "CASCADE",
+
+})
+
+Blog.belongTo(User, {
+    foreignKey: "user_id",
+
+})
+
+Comment.belongTo(User, {
+    foreignKey: "user_id",
+
+})
+
+Comment.belongTo(Blog, {
+    foreignKey: "blog_id",
+
+})
+
+
+module.exports = { User, Blog, Comment };
 
