5f286b8c3feeaee74ec2904100a0999b208366c4
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-05-25 21:47:59 -0700

    add homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { Blog } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
     // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const data = await Blog.findAll({
+     
     });
 
     // Serialize user data so templates can read it
-    const users = userData.map((project) => project.get({ plain: true }));
+    const blogs = data.map((project) => project.get({ plain: true }));
 
+    const logged_in = req.session.logged_in
     // Pass serialized data into Handlebars.js template
-    res.render('homepage', { users });
+    res.render('homepage', { blogs,logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
