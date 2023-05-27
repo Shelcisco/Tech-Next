7735ed7b88a041e3237f016b4686ebb95cf63249
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-05-27 13:29:59 -0700

    ADD GET dashboard/blog id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Blog } = require('../models');
+const { Blog, User } = require('../models');
+const {withAuth, withoutAuth} = require("../utils/auth")
 
 router.get('/', async (req, res) => {
   try {
@@ -18,5 +19,62 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Get all users, sorted by name
+    const data = await Blog.findAll({
+     where: {
+      user_id: req.session.user_id
+     }
+    });
+
+    
+
+    // Serialize user data so templates can read it
+    const blogs = data.map((project) => project.get({ plain: true }));
+
+    const logged_in = req.session.logged_in
+    // Pass serialized data into Handlebars.js template
+    res.render('dashboard', { blogs,logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const target = req.params.id;
+    // Get all users, sorted by name
+    const data = await Blog.findByPk({
+      target, 
+      include: [{model:User}, {model:Comment}]
+    });
+const blog = data.get({plain:true})
+    
+
+    const logged_in = req.session.logged_in
+    if (logged_in&&req.session.user_id==blog.user_id){
+      res.render('edit-blog', {...blog,logged_in }); 
+    }
+    else {
+// Pass serialized data into Handlebars.js template
+    res.render('view-blog', { ...blog,logged_in });
+  }
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get ('/login', withoutAuth, async (req,res) => {
+try{
+  const logged_in = req.session.logged_in
+  res.render('login', {logged_in});
+}
+catch (err) {
+  res.status(500).json(err);
+}
+})
 
 module.exports = router;
