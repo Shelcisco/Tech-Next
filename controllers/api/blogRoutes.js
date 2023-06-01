8d85bc62e4190f44f2e0a0f7d050526c56f4d8aa
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-05-31 19:25:07 -0700

    FIX typos

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -26,7 +26,7 @@ router.get('/:id', async(req, res) => {
   
       res.status(200).json(data);
     } catch (err) {
-      res.status(500).json(err);
+      res.status(400).json(err);
     }
 });
 
